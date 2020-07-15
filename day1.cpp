ed5a31235f0f2d2e71504a14e35ac409ac3391b5
NatsuNoS7 <68296512+NatsuNoS7@users.noreply.github.com>
2020-07-15 17:15:15 +0900

    Create day1.cpp
    day1 : cpp

---
+++ day1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std ::cout << "Test Output" << std::endl;
+    std ::cout << "아에이오우" << std::endl;
+
+    cout << "std namespace 사용" << endl;
+
+    return 0;
+}
