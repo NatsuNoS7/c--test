2d0b6c193110ae6173e27b23b013e86458994297
NatsuNoS7 <68296512+NatsuNoS7@users.noreply.github.com>
2020-07-20 17:15:14 +0900

    Create day2.cpp
    day2 cpp

---
+++ day2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Number = 10;
+
+    Number = 20;
+
+    cout << Number << endl;
+
+    char cTest = 't';
+
+    cout << cTest << endl;
+
+    float fNumber = 3.14f;
+
+    cout << fNumber << endl;
+
+    double dNumber = 123.4567;
+
+    cout << dNumber << endl;
+
+    return 0;
+}
